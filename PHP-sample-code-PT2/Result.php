ae7636a32597d15158aaf4f2ea3f387d401bc555
Walaa Elsaeed <walaa.elsaeed@elgameya.net>
2021-03-03 00:09:33 +0200

    remove calling the plugin in the result page

--- PHP-sample-code-PT2/Result.php
+++ PHP-sample-code-PT2/Result.php
@@ -1,10 +1,9 @@
 <?php
-require_once 'Paytabs-plugin.php';
-$plugin = new Paytabs();
 
 $response_data = $_POST;
 
 $transRef = filter_input(INPUT_POST, 'tranRef');
+
 if (!$transRef)
     die('Transaction reference is not set. return url must be HTTPs with POST method to can retrieve data');
 
