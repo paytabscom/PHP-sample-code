a8136a5c1ae9fd268f2b6c8e48b193e501c858cc
Walaa Elsaeed <walaa.elsaeed@elgameya.net>
2021-03-21 13:59:57 +0200

    validate payment with signature and remove test folder

--- PHP-sample-code-PT2/Result.php
+++ PHP-sample-code-PT2/Result.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'Paytabs-plugin.php';
+$plugin = new Paytabs();
 
 $response_data = $_POST;
 
@@ -10,4 +12,49 @@ if (!$transRef)
 print_r('Payment updates');
 print_r('<br>');
 print_r($response_data);
+
+//
+
+echo "<h2> Transaction reference: <b>{$transRef}</b> </h2>";
+
+
+/** Local Verify PayTabs response By signature calculation: Option 1 */
+
+echo "<h3>Local Payment validation</h3>";
+
+$is_valid = $plugin->is_valid_redirect($response_data);
+
+if (!$is_valid) {
+    die('Not a valid PayTabs response');
+}
+
+//a
+
+$is_success = $response_data['respStatus'] === 'A';
+if ($is_success) {
+    echo 'Payment succeed <br>';
+    print_r($response_data);
+} else {
+    echo 'Payment failed <br>';
+}
+
+
+/** Verify PayTabs response By sending new  request: Option 2 */
+
+echo "<h3>Remote Payment validation</h3>";
+
+$request_url = 'payment/query';
+$data = [
+    "tran_ref" => $transRef
+];
+$verify_result = $plugin->send_api_request($request_url, $data);
+$is_success = $verify_result['payment_result']['response_status'] === 'A';
+if ($is_success) {
+    echo 'Payment succeed <br>';
+    print_r($verify_result);
+} else {
+    echo 'Payment failed <br>';
+}
+
+
 
